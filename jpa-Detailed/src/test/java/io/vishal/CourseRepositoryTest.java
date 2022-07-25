c9e9e8ed810e4edad0222f1efb178c9306a5030f
vishal <vishalambekar459@gmail.com>
2022-07-25 23:00:15 +0530

    pagination added in test without sorting

--- jpa-Detailed/src/test/java/io/vishal/CourseRepositoryTest.java
+++ jpa-Detailed/src/test/java/io/vishal/CourseRepositoryTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import io.vishal.entity.Course;
 import io.vishal.entity.Teacher;
@@ -22,7 +24,7 @@ public class CourseRepositoryTest {
 		System.out.println("courses list :: "+courses);
 	}
 	
-	@Test
+	//@Test
 	public void saveCourseWithTeacher() {
 		
 		Teacher teacher =Teacher.builder()
@@ -37,5 +39,38 @@ public class CourseRepositoryTest {
 				.build();
 		
 		courseRepository.save(course);
+	}
+	
+	@Test
+	public void findAllPagination() {
+		Pageable firstPageWithThreeRecords = PageRequest.of(0,3);
+		Pageable secondPageWithTwoRecords = PageRequest.of(1, 2);
+
+		
+		List<Course> courses_firstPageWithThreeRecords = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+		System.out.println("courses_firstPageWithThreeRecords :: "+courses_firstPageWithThreeRecords);
+
+		List<Course> courses_second = courseRepository.findAll(secondPageWithTwoRecords).getContent();
+		System.out.println("courses_second :: "+courses_second);
+		
+		
+		
+		
+		//total elements and pages if we go by page 1 logic
+		long totalElements_page1Logic = courseRepository.findAll(firstPageWithThreeRecords).getTotalElements();
+		System.out.println("total elements page1 logic :: "+totalElements_page1Logic);
+		
+		long totalPages_byPage1Logic = courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+		System.out.println("total pages page1 logic :: "+totalPages_byPage1Logic);
+		
+		
+		
+		
+		//total elements and pages if we go by page 2 logic
+		long totalElements_page2Logic = courseRepository.findAll(secondPageWithTwoRecords).getTotalElements();
+		System.out.println("total elements page2 logic:: "+totalElements_page2Logic);
+				
+		long totalPages_byPage2Logic = courseRepository.findAll(secondPageWithTwoRecords).getTotalPages();
+		System.out.println("total pages page2 logic:: "+totalPages_byPage2Logic);
 	}
 }
